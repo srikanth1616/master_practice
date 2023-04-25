f24fcb389735b0bca7ab30b735d57cb1575d9429
srikanth1616 <email2.srikanthb@gmail.com>
2023-04-25 10:48:15 +0530

    dynamodb

---
+++ dynamoddb/lib/dynamoddb-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+export class DynamoddbStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    // dynamodb table
+    const table = new dynamodb.Table(this, 'Table', {
+  partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+});
+    // example resource
+    // const queue = new sqs.Queue(this, 'DynamoddbQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
